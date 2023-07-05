1887b5f175f953bf7808285817ff83db96385a06
Vinh Hoang <vinhhn3@fe.edu.vn>
2023-07-05 14:52:58 +0700

    Init files

---
+++ app/src/main/java/com/example/comp1786_l3_4_android_gui/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.comp1786_l3_4_android_gui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
